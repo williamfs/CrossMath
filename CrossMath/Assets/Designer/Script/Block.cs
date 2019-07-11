c253c127d45da74a45c66b5b2d79508e1a2bdcf6
Chenfeng Zhang <akupaqi123@gmail.com>
2019-07-11 14:42:42 -0400

    Update scripts

--- CrossMath/Assets/Designer/Script/Block.cs
+++ CrossMath/Assets/Designer/Script/Block.cs
@@ -5,20 +5,16 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     public float value;
-
     public Vector3 orPosition;
-
     public bool inBlock;
-
 
     void Start()
     {
         orPosition = this.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(value);
+        //Debug.Log(value);
     }
 }
